84641107372cb3c076f4ba6a8a16849582fbabd5
David <107624642+David0l1@users.noreply.github.com>
2022-11-20 09:56:17 +0700

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -4,16 +4,6 @@ tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Unable to get NGROK t
 :check
 ping 127.0.0.1 > null
 cls
-echo ●○○○
-echo ○●○○
-echo ○○●○
-echo ○○○●
-echo ●○○○
-echo ○●○○
-echo ○○●○
-echo ○○○●
-echo ●○○○
-echo ○●○○
-echo ○○●○
-echo ○○○●
+echo Hi!
+echo Hi!
 goto check
