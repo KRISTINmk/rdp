2841d0afb68e26b81d905c8391c04246df5f27e8
davidpangrib001 <davidpangrib001@gmail.com>
2022-12-18 13:27:10 +0700

    Resource RDP

---
+++ loop.bat
@@ -0,0 +1,9 @@
+@echo off
+echo RDP Build Succesfully!
+tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels" & ping 127.0.0.1 >Nul & exit
+:check
+ping 127.0.0.1 > null
+cls
+echo RDP Siap Digunakan :D
+echo Cuma Nyala 1 Jam Aja..
+goto check
